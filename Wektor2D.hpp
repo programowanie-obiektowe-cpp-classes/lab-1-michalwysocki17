5ee3c49107c8a7184321dd3ab67241bae6b10cb3
Michał Wysocki <01181184@pw.edu.pl>
2025-10-15 22:05:15 +0200

    dziala

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,27 @@
+#include <print>
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D(double poz = 0, double pion = 0)
+    {
+        x = poz;
+        y = pion;
+    }
+    ~Wektor2D() {}
+    void setX(double a) { x = a; }
+    double  getX() { return x; }
+    void setY(double a) { y = a; }
+    double  getY() { return y; }
+ private:
+    double x, y;
 };
+
+Wektor2D operator+(Wektor2D v1, Wektor2D v2)
+{
+    return Wektor2D(v2.getX() + v1.getX(), v2.getY() + v1.getY());
+}
+double operator*(Wektor2D v1, Wektor2D v2)
+{
+    return ((v2.getX() * v1.getX()) + (v2.getY() * v1.getY()));
+}
